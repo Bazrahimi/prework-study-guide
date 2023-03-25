4cc1827b1875a3104f38e06e5109944dad057e76
Baz Rahimi <bazrahimi@hotmail.com>
2023-03-26 03:38:24 +1100

    inserted commenting line on conditional function

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
-
 var topics = [ 'HTML', 'CSS', 'Git', 'JavaScript'];
+
   for (var x = 0; x < topics.length; x++) {
   console.log(topics[x]);
 }
 
+
+/*
 if (topic ==='HTML'){
    console.log("let's study HTML!");
 } else if (topic ==='CSS'){
@@ -15,6 +17,6 @@ if (topic ==='HTML'){
 } else {
   console.log('Please try again!');
 }
-
+*/
 
 
